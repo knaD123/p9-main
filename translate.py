648758614c2b95aad62f3e3bc15b99e11bd17aa5
knaD123 <amad18@student.aau.dk>
2022-10-07 09:13:05 +0200

    add demand weight

---
+++ translate.py
@@ -0,0 +1,56 @@
+import json
+import os
+import re
+
+if not os.path.exists("demands"):
+    os.mkdir("demands")
+
+for topo in os.listdir("zoo"):
+    f = open("zoo/" + topo, "r")
+    data = f.read()
+    f.close()
+    data_into_list = data.replace(" ", "").split("\n")
+    filtered_list = list(dict.fromkeys(data_into_list))
+    i = 0
+    for line in filtered_list:
+        if line.__contains__("],"):
+            break
+        i += 1
+    filtered_list_sliced = filtered_list[2:i]
+    templist = []
+    for i in filtered_list_sliced:
+        result = re.sub(r'[^_A-Za-z0-9]', '', i)
+        result = re.sub(r'[0-9_]*[^A-Za-z0-9]', '', result)
+        templist.append(result)
+
+    kvplaces = {}
+    for idx in enumerate(templist):
+        kvplaces.update({idx[0]: idx[1]})
+
+    for demand in os.listdir("zoo_demands"):
+        if demand.__contains__(os.path.splitext(topo)[0]):
+            f = open("zoo_demands/" + demand, "r")
+            file = f.readlines()
+            f.close()
+            finallst = []
+            file = file[2:]
+
+            for line in file:
+                temptriple = []
+                line = re.sub(r'[A-Za-z]*','', line)
+                line = re.sub(r'\n', '', line)
+                line = re.split(" ",line)
+                line = line[1:]
+                line = [int(i) for i in line]
+                temptriple.append(kvplaces.get(line[0]))
+                temptriple.append(kvplaces.get(line[1]))
+                temptriple.append(line[2])
+                finallst.append(temptriple)
+
+            stringlst = str(finallst).replace("'", "")
+            demandsplit = demand.split(".")
+            filename = demandsplit[0] + demandsplit[1] + ".yml"
+
+            newfile = open("demands/" + filename, "a")
+            newfile.write(stringlst)
+            newfile.close()
