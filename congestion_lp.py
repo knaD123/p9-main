c3fd21ee3382b26749c95507585ee913ee0fd231
knaD123 <amad18@student.aau.dk>
2022-11-16 14:40:40 +0100

    Create results for congestion lp

--- congestion_lp.py
+++ congestion_lp.py
@@ -1,7 +1,49 @@
+import json
+import math
+import os
+import yaml
 from ortools.linear_solver import pywraplp
+import sys
+from mpls_fwd_gen import *
 
 
-def congestion_lp(graph, capacities, demands, printflows=False, singleflow=False):  # Inputs networkx directed graph, dict of capacities, dict of demands
+def main(graphfile, demandsfile, print_flows=False, unsplittable_flow=False):
+    with open("demands/" + demandsfile, "r") as file:
+        flows_with_load = [[x, y, int(z)] for [x, y, z] in yaml.load(file, Loader=yaml.BaseLoader)]
+
+    # Sort the flows
+    flows_with_load = sorted(flows_with_load, key=lambda x: x[2], reverse=True)[:math.ceil(len(flows_with_load))]
+
+    with open(graphfile) as f:
+        topo_data = json.load(f)
+
+    # Load link capacities
+    link_caps = {}
+    for f in topo_data["network"]["links"]:
+        src = f["from_router"]
+        tgt = f["to_router"]
+        if src != tgt:
+            link_caps[(src,tgt)] = f.get("bandwidth", 0)
+            if f["bidirectional"]:
+                link_caps[(tgt, src)] = f.get("bandwidth", 0)
+
+    graph = topology_from_aalwines_json(graphfile, visualize=False).to_directed()
+
+    minimized_max_util = congestion_lp(graph, link_caps, flows_with_load)
+
+    result_folder = "results/lp/" + graphfile.split("/")[1]
+    os.makedirs(result_folder, exist_ok=True)
+    result_file = os.path.join(result_folder, "default")
+
+    results = dict()
+    results["topology"] = graphfile
+    results["demandfile"] = demandsfile
+    results["minimal_maximal_utilization"] = minimized_max_util
+
+    with open(os.path.join(result_folder, "results.json"), "w") as f:
+        json.dump(results, f, indent=4)
+
+def congestion_lp(graph, capacities, demands, print_flows=False, unsplittable_flow=False):  # Inputs networkx directed graph, dict of capacities, dict of demands
     def demand(i, d):
         if demands[d][0] == i:  # source
             return 1
@@ -10,18 +52,16 @@ def congestion_lp(graph, capacities, demands, printflows=False, singleflow=False
         else:
             return 0  # intermediate
 
-    if singleflow:
+    if unsplittable_flow:
         solver = pywraplp.Solver.CreateSolver('SCIP')
     else:
         solver = pywraplp.Solver.CreateSolver('GLOP')
-    if not solver:
-        return
 
     # alpha is the minimal coefficient for capacities needed s.t. all demands do not exceed capacities
     alpha = solver.NumVar(0, solver.infinity(), "alpha")
 
     # Flow variables for solver
-    if singleflow:
+    if unsplittable_flow:
         f = {(i, j, d): solver.IntVar(0, 1, "{}->{}=>{}->{}".format(i, j, demands[d][0], demands[d][1])) for
              (i, j) in graph.edges for d in range(len(demands))}
     else:
@@ -49,7 +89,7 @@ def congestion_lp(graph, capacities, demands, printflows=False, singleflow=False
     status = solver.Solve()
 
     if status == pywraplp.Solver.OPTIMAL:
-        if printflows:
+        if print_flows:
             for d in range(len(demands)):
                 for (i, j) in graph.edges:
                     if f[i,j,d].SolutionValue() > 0:
@@ -59,3 +99,6 @@ def congestion_lp(graph, capacities, demands, printflows=False, singleflow=False
     else:
         print(solver.Objective().Value())
         print('The problem does not have an optimal solution.')
+
+if __name__ == "__main__":
+    main(sys.argv[1], sys.argv[2])
