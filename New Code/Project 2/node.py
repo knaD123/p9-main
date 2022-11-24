b14b52b528e303e4907ef906081ab8ba32e1e115
Aixene <56993791+Aixene@users.noreply.github.com>
2022-11-24 12:16:38 +0100

    fixes

--- New Code/Project 2/node.py
+++ New Code/Project 2/node.py
@@ -17,24 +17,28 @@ class Node:
         self.jumpsfromtarget = 0
         return
 
-    def newLink(self, n, i):
-        l1 = Link(i, self, n)
+    def newLink(self, n, i1, i2 = None):
+        l1 = Link(i1, self, n)
+        if i2 != None:
+            l2 = Link(i2, n, self)
+        else:
+            l2 = Link(i1, n, self)
         self.clinks.append(l1)
-        n.clinks.append(l1)
+        n.clinks.append(l2)
 
 class Link:
     def __init__(self, i, n1, n2):
         self.capacity = i
         self.usage = 0
-        self.node1 = n1
-        self.node2 = n2
+        self.source_node = n1
+        self.target_node = n2
         return
 
     def nextnode(self, node):
-        if node == self.node1:
-            return self.node2
-        if node == self.node2:
-            return self.node1
+        if node == self.source_node:
+            return self.target_node
+        if node == self.target_node:
+            return self.source_node
         return
 
 
