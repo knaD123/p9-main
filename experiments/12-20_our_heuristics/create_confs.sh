c90740a91c988934c0e20e45626b23389e78043a
lassebk45 <lkar18@student.aau.dk>
2022-12-20 12:44:19 +0100

    experiments for all our heuristics

---
+++ experiments/12-20_our_heuristics/create_confs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --partition=naples
+
+EXECUTOR="sbatch"
+if [ "$1" = "no" ]; then
+  EXECUTOR=""
+fi
+
+FILTER="$2"
+
+source venv/bin/activate
+
+rm confs/*/conf*
+
+for TOPO in $(ls topologies/${FILTER}*) ; do
+  $EXECUTOR experiments/12-20_our_heuristics/run-createconfs.sh ${TOPO}
+done
+
