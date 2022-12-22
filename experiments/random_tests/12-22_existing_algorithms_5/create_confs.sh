7b902539fcf4031ab31ea92987cc218a4df7e41e
Lassebk45 <49187473+Lassebk45@users.noreply.github.com>
2022-12-22 12:47:42 +0100

    random experiments batch 1

---
+++ experiments/random_tests/12-22_existing_algorithms_5/create_confs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --partition=naples
+
+source venv/bin/activate
+
+for TOPO in $(ls topologies/$zoo_A*) ; do
+  sbatch experiments/random_tests/12-22_existing_algorithms_5/run-createconfs.sh ${TOPO}
+done
