764757051c6d91e95307266568c90ad6f7d5d84d
Lassebk45 <49187473+Lassebk45@users.noreply.github.com>
2022-12-22 14:26:11 +0100

    more exp

---
+++ experiments/random_tests/12-22_existing_algorithms_threshold_1000_5/schedule-run-tool.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for TOPO in $(ls confs_12-22_existing_algorithms_1) ; do
+    for CONF in $(ls confs_12-22_existing_algorithms_1/${TOPO}/conf*) ; do
+        sbatch experiments/random_tests/12-22_existing_algorithms_1/run-tool.sh ${CONF}
+    done
+done
