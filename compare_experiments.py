a9caaabe2fc5ebcd4bc81fd56f9c8f45e4e42cdf
Lassebk45 <lkar18@student.aau.dk>
2022-12-25 13:55:12 +0100

    small fix

--- compare_experiments.py
+++ compare_experiments.py
@@ -10,12 +10,15 @@ experiments = [x for x in listdir(res_dir) if isdir(join(res_dir, x))]
 
 unfinished_topologies = []
 for topology in all_topologies:
+    break_topo = False
     for experiment in experiments:
         for alg in [x for x in listdir(join(res_dir, experiment)) if isdir(join(res_dir, experiment, x))]:
-            if not topology in listdir(join(res_dir, experiment)):
+            if not topology in listdir(join(res_dir, experiment, alg)):
                 unfinished_topologies.append(topology)
+                break_topo = True
+                break
+        if break_topo:
+            break
 
 finished_topologies = [x for x in all_topologies if x not in unfinished_topologies]
-
 print(finished_topologies)
-
