7bcd6231ffcdf6d4599ffcda5a3a2f6016b1350b
lasse <lkar18@student.aau.dk>
2023-02-27 12:18:27 +0100

    added argument for zero latency

--- to_omnet.py
+++ to_omnet.py
@@ -37,7 +37,7 @@ def main(conf):
     name = re.search(r".*zoo_(.*)\.json", conf["topology"]).group(1).lower()
     network.flows_for_omnet = network.build_flow_table(flows_with_load)
 
-    network.to_omnetpp(name=name, output_dir=f"./omnet_files/{name}", scaler=conf['scaler'], packet_size=conf["packet_size"])
+    network.to_omnetpp(name=name, output_dir=f"./omnet_files/{name}", scaler=conf['scaler'], packet_size=conf["packet_size"], zero_latency=conf["zero_latency"])
 
 def num_packets(flows_with_load):
     sum = 0
@@ -75,6 +75,7 @@ if __name__ == "__main__":
     p.add_argument("--take_percent", type=float, default=1, help="What percentage of biggest flows to take")
     p.add_argument("--scaler", type=float, default=1, help="Multiplies the send interval by the scaler value and divides the link bandwidth by the same value")
     p.add_argument("--packet_size", type=int, default=64, help="Size in bytes")
+    p.add_argument("--zero_latency", action="store_true", help="Set latency to 0 for all links")
 
     conf = vars(p.parse_args())
 
